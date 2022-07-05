8aef78e2f443c6209ecbd24e17396ba01b00f604
xiaotai5109 <1048340205@qq.com>
2022-07-06 02:02:05 +0800

    利用Hystrix Dashboard实现服务监控及页面

---
+++ hystrix-dashboard/src/main/java/com/test/HystrixApplication.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author az
+ * @description
+ * @date 2022/7/6 0006
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+}
