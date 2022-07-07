c580d0dd091cf536a25fb486f4c84d6d102cf00e
xiaotai5109 <1048340205@qq.com>
2022-07-08 03:44:28 +0800

    配置中心服务端配置

--- config-server/src/main/java/com/test/ConfigApplication.java
+++ config-server/src/main/java/com/test/ConfigApplication.java
@@ -1,8 +1,19 @@
-package com.test;/**
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
  * @author az
- * @description 
+ * @description
  * @date 2022/7/8 0008
  */
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigApplication {
-    
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }
