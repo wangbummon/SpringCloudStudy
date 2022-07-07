f4dbaf902bbc44d918b3a24e50b84870348661d6
xiaotai5109 <1048340205@qq.com>
2022-07-08 01:38:41 +0800

    实现通过路由网关访问服务

---
+++ gateway-server/src/main/java/com/test/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author az
+ * @description
+ * @date 2022/7/7 0007
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
